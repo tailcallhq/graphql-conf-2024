cd18c6eb3d73d1246d5c571bed7bbaf1d5664d91
neo773 <neo773@protonmail.com>
2024-09-23 02:24:58 +0530

    fix: h1

--- preprocess-markdown.js
+++ preprocess-markdown.js
@@ -2,7 +2,10 @@ import fs from 'fs/promises';
 
 async function preprocessMarkdown() {
   const input = await fs.readFile('./docs/slides.md', 'utf-8');
-  const processed = input.replace(/^##\s*(.*?)$/gm, '<span class="h2">$1</span>');
+  const processed = input
+    .replace(/^##\s*(.*?)$/gm, '<p class="h2">$1</p>')
+    .replace(/^#\s*(.*?)$/gm, '<p class="h1">$1</p>');
+  
   await fs.writeFile('./docs/processed-slides.md', processed);
 }
 
